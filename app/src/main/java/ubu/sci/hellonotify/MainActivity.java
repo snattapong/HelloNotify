aa5c7888ef7e20cc3fefb08553926e762b55987a
Nattapong Suebsuk <nattapong.s@ubu.ac.th>
2018-09-23 13:34:36 +0700

    Notification with styles

--- app/src/main/java/ubu/sci/hellonotify/MainActivity.java
+++ app/src/main/java/ubu/sci/hellonotify/MainActivity.java
@@ -2,6 +2,11 @@ package ubu.sci.hellonotify;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -37,20 +42,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendOnChannel1(){
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_doggy);
+
+        Intent contentIntent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent =  PendingIntent.getActivity(this,0,contentIntent,0);
+
+        Intent btIntent = new Intent(this,NotificationReceiver.class);
+        btIntent.putExtra("toastMessage",tv.getText().toString());
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this,0,btIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification= new NotificationCompat.Builder(this,CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_child)
                 .setContentText(tv.getText().toString())
+                .setLargeIcon(largeIcon)
+                .setStyle(
+                        new NotificationCompat.BigTextStyle()
+                            .setBigContentTitle("Big Title")
+                            .bigText("This is ajsklfjskdjf;ajdflksajflds asfjksfjsf new sstylte heree is a very good indeed")
+                            .setSummaryText("This is a summaryText")
+                )
                 .setContentTitle(title.getText().toString())
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(pendingIntent)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setColor(Color.BLUE)
+                .addAction(R.mipmap.ic_launcher,"Action JJ",actionIntent)
                 .build();
         manager.notify(1,notification);
     }
 
     private void sendOnChannel2(){
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_doggy);
         Notification notification= new NotificationCompat.Builder(this,CHANNEL_2_ID)
+                .setLargeIcon(largeIcon)
                 .setSmallIcon(R.drawable.ic_baby)
                 .setContentText(tv.getText().toString())
+                .setStyle( new NotificationCompat.InboxStyle()
+                        .addLine("This is a line 1")
+                        .setSummaryText("Summary in box")
+                        .addLine("Oh yeah ")
+                        .addLine("Gooo d goood")
+                )
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setColor(Color.GREEN)
                 .setContentTitle(title.getText().toString())
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
